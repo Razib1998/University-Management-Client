1956ee781638d698aef9a8993bcb7deb5bee8a38
Md   Razib Sarker <mdrajibsorkar965@gmail.com>
2024-09-26 17:53:32 +0200

    Complete components setup and create a sidebar using ant design

---
+++ src/components/layouts/MainLayout.tsx
@@ -0,0 +1,83 @@
+import { Layout, Menu, MenuProps } from "antd";
+const { Header, Content, Footer, Sider } = Layout;
+
+const items: MenuProps["items"] = [
+  {
+    key: "1",
+    label: "Dashboard",
+  },
+  {
+    key: 2,
+    label: "Profile",
+  },
+  {
+    key: 3,
+    label: "User Management",
+    children: [
+      {
+        key: 11,
+        label: "Create Admin",
+      },
+      {
+        key: 22,
+        label: "Create Student",
+      },
+      {
+        key: 33,
+        label: "Create Faculty",
+      },
+    ],
+  },
+];
+const MainLayout = () => {
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="50"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div
+          style={{
+            color: "white",
+            height: "4rem",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h1>My University</h1>
+        </div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["4"]}
+          items={items}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0 }} />
+        <Content style={{ margin: "24px 16px 0" }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+            }}
+          >
+            All content should be Here...
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default MainLayout;
