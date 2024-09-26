db992784d817d8c402bb6cc55d670ab634bb16d0
Md   Razib Sarker <mdrajibsorkar965@gmail.com>
2024-09-26 22:13:34 +0200

    create a connection with redux

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -2,6 +2,7 @@ import { Layout, Menu } from "antd";
 import { Outlet } from "react-router-dom";
 import { adminSidebarItems } from "../../routes/admin.routes";
 const { Header, Content, Footer, Sider } = Layout;
+
 // Hard coded Way..
 // const items: MenuProps["items"] = [
 //   {
